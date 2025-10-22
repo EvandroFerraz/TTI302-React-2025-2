212c6121996c2794aa6aae8792a6022277b77e8d
evandro <ferraz.evandro@outlook.com>
2025-10-21 22:06:39 -0300

    ap03 concluída, iniciamos a próxima na apostila 04

--- meu-primeiro-app/src/index.js
+++ meu-primeiro-app/src/index.js
@@ -2,10 +2,26 @@ import React from 'react'
 // cria a raiz da aplicação, que se refere ao ponto da arvore DOM onde o código
 // de <App/> será carregado
 import { createRoot } from 'react-dom/client'
+import "./styles.css"
 
 const App = () => {
     // JSX, é uma linguagem que mistura JavaScript com HTML
-    return <div> Meu primeiro componente React </div>
+    const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}
+    const estilosDiv = {margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}
+    const estilosLabel = {display: 'block', marginBottom:4};
+    const estilosInput = {paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden',width: '100%', borderRadius: 8, outline: 'none', boxSizing:'border-box'}
+    const textoRotulo = "Nome:"
+    const obterTextoDoBotao = () => {
+        return "Enviar";
+    }
+
+    return(
+        <div style={estilosDiv}>
+            <label className="rotulo" htmlFor="nome" style={estilosLabel}>{textoRotulo}</label>
+            <input type="text" id="nome" style={estilosInput}/>
+            <button style={estilosBotao}>{obterTextoDoBotao()}</button>
+        </div>
+    )
 }
 
 //render = renderizar/construir a interface gráfica a partir do componente
