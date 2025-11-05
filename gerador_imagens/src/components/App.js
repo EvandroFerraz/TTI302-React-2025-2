28eb32b254b62d37b564820d7b27d4a83787c747
evandro <ferraz.evandro@outlook.com>
2025-11-04 22:24:48 -0300

    ap07: pagina 22

--- gerador_imagens/src/components/App.js
+++ gerador_imagens/src/components/App.js
@@ -1,8 +1,33 @@
 import React, {Component} from 'react'
 import Busca from "./Busca"
+import env from 'react-dotenv'
+
+// importamos o método para criar um cliente de conexão à api do pexels
+import {createClient} from 'pexels'
 
 export default class App extends Component {
+    // state = {}
+
+    onBuscaRealizada = (termo) => {
+        this.pexelsCliente.photos.search(
+            {
+                query: termo
+            }
+        ).then(pics => console.log(pics))
+    }
+
+    pexelsCliente = null
+    // método de ciclo de vida que é executado no momento em que o componente
+    // é carregado pela primeira vez
+    componentDidMount(){
+        // pexelsCliente se torna um objeto que representa a conexao estabelecida
+        // com a api do pexels
+        this.pexelsCliente = createClient(env.PEXELS_KEY)
+    }
+
     render(){
+        // imprime o valor guardado na variável PEXELS_KEY dentro do .env na raiz do projeto
+        console.log(env.PEXELS_KEY)
         // retorna JSX
         return(
             <div className="grid justify-content-center m-auto w-9 border-round border-1 border-400">
@@ -10,7 +35,7 @@ export default class App extends Component {
                     <h1>Exibir uma lista de ... </h1>
                 </div>
                 <div className="col-8">
-                    <Busca/>
+                    <Busca onBuscaRealizada={this.onBuscaRealizada}/>
                 </div>
             </div>
         )
--- gerador_imagens/src/components/Busca.js
+++ gerador_imagens/src/components/Busca.js
@@ -1,9 +1,13 @@
-// componente de classe
-// estado do componente
-
 import React, {Component} from 'react'
 import { InputText } from 'primereact/inputtext'
 import { Button } from 'primereact/button'
+
+// componente funcional
+// const X = {return JSX}
+
+// componente de classe vai ser representado por uma classe
+// um componente de classe tem como vantagem a existencia de um estado
+// 
 
 export default class Busca extends Component {
     state = {
@@ -11,28 +15,45 @@ export default class Busca extends Component {
     }
 
     onTermoAlterado = (event) => {
+        //event.target = <InputText/>
+        // .value = valor atual escrito no componente <InputText/>
         console.log(event.target.value)
+        // agora como guardamos o valor tual do campo de texto dentro do estado do componente, podemos dizer
+        // que Busca é um componente controlado
+        this.setState({termoDeBusca: event.target.value})
     }
 
+    onFormSubmit = (event) => {
+        // cancela o comportamento padrão do forms
+        // não deixa o navegador submeter o form automaticamente
+        event.preventDefault()
+        this.props.onBuscaRealizada(this.state.termoDeBusca)
+    }
+
+    // método render() é chamado toda vez que existe alguma alteração no estado do componente
     render(){
         return(
-            // empilhando os filhos da div
-            <div className="flex flex-column">
-                {/* ícone à esquerda, largura máxima */}
-                <span className="p-input-icon-left w-full">
-                    {/* pi-search = ícone de lupa */}
-                    <i className="pi pi-search"/>
-                    <InputText
-                        className="w-full"
-                        onChange={this.onTermoAlterado}
-                        placeholder={this.props.dica}
+            // Código JSX correspondente ao componente Busca
+            <form onSubmit={this.onFormSubmit}>
+                {/* empilhando os filhos da div */}
+                <div className="flex flex-column">
+                    {/* ícone à esquerda, largura máxima */}
+                    <span className="p-input-icon-left w-full">
+                        {/* pi-search = ícone de lupa */}
+                        <i className="pi pi-search"/>
+                        <InputText
+                            value={this.state.termoDeBusca}
+                            className="w-full"
+                            onChange={this.onTermoAlterado}
+                            placeholder={this.props.dica}
+                        />
+                    </span>
+                    <Button
+                        label="OK"
+                        className="p-button-outlined mt-2"
                     />
-                </span>
-                <Button
-                    label="OK"
-                    className="p-button-outlined mt-2"
-                />
-            </div>
+                </div>
+            </form>
         )
     }
 }
